6d1f21e23c9fe4d057cfa9a58914361d78ea1670
John Doe <778zniaz@gmail.com>
2024-09-27 22:30:15 +0300

    инициализация

--- src/script.js
+++ src/script.js
@@ -1,7 +1,15 @@
 const tap = document.getElementsByClassName('tap');
 const calc = document.getElementsByClassName('calc');
 var i = 0;
+if (localStorage.getItem('taps')>0) {
+    calc[0].innerHTML = localStorage.getItem('taps');
+    i= localStorage.getItem('taps');
+} else {
+    i=0;
+}
+
 tap[0].addEventListener('click', ()=> {
- i = i+1;
-calc[0].innerHTML = i;
-})
+    i = i-(-1);
+    localStorage.setItem('taps', i)
+    calc[0].innerHTML = localStorage.getItem('taps');
+});
